1096da77001ee80354b650c91fd473c8577d0796
Ovidiu Popoviciu <ovidiup13.tuta.io>
2017-10-14 17:24:57 +0100

    Add issue query for Github api

--- scripts/enrich_repos.js
+++ scripts/enrich_repos.js
@@ -1,13 +1,12 @@
-const Promise = require("bluebird").Promise;
-const rp = require("request-promise");
-
 const mongoose = require("../db/db");
 const RepositoryDAO = require("../db/dao/repo.dao");
 
 const {
     GraphQLClient
 } = require("graphql-request");
+
 const queryPR = require("./queries/pr_query");
+const queryIssue = require("./queries/issue_query");
 
 const client = new GraphQLClient(process.env.GITHUB_API_V4_URL, {
     headers: {
@@ -16,10 +15,20 @@ const client = new GraphQLClient(process.env.GITHUB_API_V4_URL, {
     }
 });
 
-client.request(queryPR.query, queryPR.variables)
+client.request(queryIssue.query("Microsoft", "vscode", 25655), queryIssue.variables)
     .then(data => {
         console.log(data);
     })
     .catch(err => {
+        console.error("Error on requesting issue info");
+        console.error(err.response.errors);
+    });
+
+client.request(queryPR.query("tensorflow", "tensorflow"), queryPR.variables)
+    .then(data => {
+        console.log(data);
+    })
+    .catch(err => {
+        console.error("Error on requesting PR info");
         console.error(err.response.errors);
     });
---
+++ scripts/queries/issue_query.js
@@ -0,0 +1,19 @@
+module.exports = {
+    query: function (owner, name, issueNumber) {
+        return `query {
+  repository(name:"${name}", owner:"${owner}") {
+    issue(number:${issueNumber}){
+      title
+      bodyText
+      closed
+      createdAt
+      updatedAt
+      author {
+        login
+      }
+    }
+  }
+}`;
+    },
+    variables: ""
+};
