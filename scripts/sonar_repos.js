f44611365adceb4391c99bf058db9331767aea48
Ovidiu Popoviciu <ovidiup13@tuta.io>
2017-10-19 22:27:25 +0100

    Add script for running sonar client against multiple commits for a repo

--- scripts/sonar_repos.js
+++ scripts/sonar_repos.js
@@ -1,4 +1,75 @@
 const NodeGit = require("nodegit");
-const mongoose = require("../db/db");
+const Repository = NodeGit.Repository;
+const Commit = Repository.Commit;
 
-module.exports = {};
+const path = require("path");
+const fs = require("fs");
+
+const mongoose = require("../db/db");
+const RepositoryDAO = require("../db/dao/repo.dao");
+const SonarClient = require("./clients/sonar_client");
+
+const srcFolder = "src";
+const tempFolder = require("../constants").TEMP_FOLDER;
+
+scanRepositories()
+    .then(() => {
+        console.log("Scan successful.");
+        // process.exit();
+    })
+    .catch(err => {
+        console.error(err);
+    });
+
+async function scanRepositories() {
+    const repoMeta = await RepositoryDAO.getRepository("facebook:react");
+    const prs = await RepositoryDAO.getPullRequests("facebook:react");
+
+    const pullRequests = prs.pullRequests;
+    pullRequests.sort(sortPRs);
+
+    const repoPath = path.join(tempFolder, repoMeta.name);
+    const repo = await Repository.open(repoPath);
+
+    return checkoutCommits(repo, repoMeta, pullRequests);
+}
+
+async function checkoutCommits(repo, repoMeta, prs) {
+
+    if (prs.length === 0) {
+        return;
+    }
+
+    const repoPath = path.join(tempFolder, repoMeta.name);
+
+    const pr = prs.shift();
+    const options = {
+        path: repoPath,
+        srcPath: path.join(repoPath, srcFolder),
+        commit: pr.mergeCommit,
+        date: pr.mergedAt,
+    };
+
+    const checkoutOps = {
+        checkoutNotify: NodeGit.Checkout.NOTIFY.ALL,
+        checkoutStrategy: NodeGit.Checkout.STRATEGY.USE_THEIRS
+    }
+
+    await repo.setHeadDetached(pr.mergeCommit);
+    await NodeGit.Checkout.head(repo, checkoutOps).then(async() => {
+
+        repo.getCurrentBranch().then(ref => {
+            console.log("On " + ref.shorthand() + " " + ref.target());
+        });
+
+        // start sonar analysis
+        await SonarClient.startSonar(repoMeta, options);
+
+        // move to next commit
+        return checkoutCommits(repo, repoMeta, prs);
+    });
+}
+
+function sortPRs(a, b) {
+    return new Date(a.mergedAt) - new Date(b.mergedAt);
+}
